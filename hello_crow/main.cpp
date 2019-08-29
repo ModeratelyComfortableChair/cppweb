b61c799bedc1ab30996000ad028309ce3db9a69f
Jerome <jerome.marfleet@mail.mcgill.ca>
2019-08-29 12:32:06 -0400

    setup mongo. Doesn't work yet. need to trouble shoot

--- hello_crow/main.cpp
+++ hello_crow/main.cpp
@@ -1,25 +1,123 @@
 #include "crow_all.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <boost/filesystem.hpp>
+
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/oid.hpp>
+
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/instance.hpp>
+
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
+using bsoncxx::builder::basic::kvp;
+using mongocxx::cursor;
+
+
 using namespace std;
 using namespace crow;
+using namespace crow::mustache;
 
-const uint16_t DEFAULT_PORT = 18080;
+string getView(const string &filename, context &x){
+  return load("../public/" + filename + ".html").render(x);
+}
+
+const uint16_t DEFAULT_PORT = 8080;
+
+void sendFile(response &res, string filename, string contentType){
+  ifstream in("../public/" + filename, ifstream::in);
+  if(in){
+    ostringstream contents;
+    contents << in.rdbuf();
+    in.close();
+    res.set_header("Content-Type", contentType);
+    res.write(contents.str());
+  } else {
+    res.code = 404;
+    res.write("Not found");
+  }
+  res.end();
+}
+
+void sendHtml(response &res, string filename){
+  sendFile(res, filename + ".html", "text/html");
+}
+
+void sendImage(response &res, string filename){
+  sendFile(res, "images/"+ filename, "image/jpeg");
+}
+
+void sendStyle(response &res, string filename){
+  sendFile(res, "styles/" + filename, "text/css");
+}
+
+void sendScript(response &res, string filename){
+  sendFile(res, "scripts/"+ filename, "text/javascript");
+}
+
 
 int main(int argc, char* argv[]){
 
   crow::SimpleApp app;
 
+  //need this for mustache to work
+  set_base(".");
+
+  mongocxx::instance inst{};
+  string mongoConnect = std::string(getenv("MONGODB_URI"));
+  mongocxx::client conn{mongocxx::uri{mongoConnect}};
+  auto collection = conn["cpp-api-tutorial"]["contacts"];
+
+  CROW_ROUTE(app, "/images/<string>")
+  ([](const request &req, response &res, string filename){
+    sendImage(res, filename);
+  });
+
+  CROW_ROUTE(app, "/styles/<string>")
+  ([](const request &req, response &res, string filename){
+    sendStyle(res, filename);
+  });
+
+  CROW_ROUTE(app, "/scripts/<string>")
+  ([](const request &req, response &res, string filename){
+    sendScript(res, filename);
+  });
+
   CROW_ROUTE(app, "/")
   ([](const request &req, response &res){
-    ifstream in("../public/index.html", ifstream::in);
-    if(in){
-      ostringstream contents;
-      contents << in.rdbuf();
-      in.close();
-      res.write(contents.str());
-    } else {
-      res.write("Not found");
+    sendHtml(res, "index");
+  });
+
+  CROW_ROUTE(app, "/about")
+  ([](const request &req, response &res){
+    sendHtml(res, "about");
+  });
+
+  CROW_ROUTE(app, "/contacts")
+  ([&collection](){
+    mongocxx::options::find opts;
+    opts.limit(10);
+    auto docs = collection.find({}, opts);
+    crow::json::wvalue dto;
+    vector<crow::json::rvalue> contacts;
+    contacts.reserve(10);
+
+
+    for(auto doc : docs){
+      contacts.push_back(json::load(bsoncxx::to_json(doc)));
     }
-    res.end();
+    dto["contacts"] = contacts;
+    return getView("contacts", dto);
   });
 
   char* port = getenv("PORT");
